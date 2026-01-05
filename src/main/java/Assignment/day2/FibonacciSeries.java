d31d912a01f118512c17b7c890f75374896cd07e
Madhan <madhanofficial08@gmail.com>
2026-01-05 22:04:11 +0530

    today commit

--- src/main/java/Assignment/day2/FibonacciSeries.java
+++ src/main/java/Assignment/day2/FibonacciSeries.java
@@ -19,9 +19,15 @@ public class FibonacciSeries
     {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        for(int i = 0;i<n;i++)
+        int n1 = 0;
+        int n2  = 1;
+        System.out.print(n1 +" "+n2+" ");
+        for(int i = 3;i<=n;i++)
         {
-            int temp = n;
+            int n3 = n1 + n2;
+            System.out.print(n3+" ");
+            n1 = n2;
+            n2 = n3;
         }
     }
 }
